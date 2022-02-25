6c1e4ea588f9b095a463994a7d3f08d5697ee543
pancho <pancho@pancho.com>
2022-02-24 23:31:32 -0300

    posman enviar messengig

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,24 @@ import 'package:pushmensage/screens/mensage_in.dart';
 import 'package:pushmensage/static/messaging.dart';
 import 'package:pushmensage/static/router.dart';
 
+//para mandar desde postman
+//en post https://fcm.googleapis.com/fcm/send
+//en headers -> Authorization ----> key=[codigo autorizacion en engranaje, cloudmessagig --> clave del servidor]
+// en body -->raw--> seleccionar JSON donde dice text
+//
+//y luego copiar cambiando to(tokens)
+//{
+//    "data":{
+//            "nombre":"pep postman"
+//           },
+//    "notification":{
+//                    "title":"postMan",
+//                    "body":"Mensaje Postman"
+//                  },
+//    "to":"d0eFu3B9RhGDgsfwKlh164:APA91bH7msEcU3HBmTh-t1jQj9eSOvboXTpiSBcKT4_wyT0-4CaR-DJBHUNVQFqLXYjdsP0zQPvi0-OAcq1-8JFl1fqBvI4amcEqFAMY4vU357vHLx7c0Peklug8m-na4GD6lkXgos9i",
+//    "priority":"high"
+//}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
